8b7829025ea01f66bf75381e537a0b5240d71435
Kaushik Narayan R <rknarayan02@gmail.com>
2024-02-11 14:01:03 -0700

    1.2b caesar done

---
+++ hw1.2/caesar_without_key.py
@@ -0,0 +1,21 @@
+with open("3/withoutkey/ciphertext.txt", "r") as caesar_cipher_file:
+    caesar_cipher_content = caesar_cipher_file.read()
+
+    for key_shift in range(1,27): # both left and right, 13x2=26
+        print("key shift", key_shift)
+        
+        caesar_plaintext_content_left_shift = ""
+        caesar_plaintext_content_right_shift = ""
+        for char in caesar_cipher_content:
+            caesar_plaintext_content_left_shift += chr(
+                (ord(char) - ord("A") - key_shift) % 26 + ord("A")
+            )
+            caesar_plaintext_content_right_shift += chr(
+                (ord(char) - ord("A") + key_shift) % 26 + ord("A")
+            )
+
+        print("left shift")
+        print(caesar_plaintext_content_left_shift)
+        print()
+        print("right shift")
+        print(caesar_plaintext_content_right_shift)
